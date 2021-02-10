b626c3de8f89db7fddb0347735040f9b8bcfab9a
Doublepor <doublepor.pp@gmail.com>
2021-02-10 16:10:20 +0700

    REST API Task_Success

---
+++ src/task-success/dto/create-task-success.dto.ts
@@ -0,0 +1,8 @@
+export class CreateTaskSuccessDto {
+    shortcode: String;
+    caption: String;
+    time_start: String;
+    time_stop: String;
+    user_id: String;
+    task_id: String;
+}
---
+++ src/task-success/entities/task-success.entity.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TaskSuccessDocument = TaskSuccess & Document;
+
+@Schema({ collection: "task_success" })
+export class TaskSuccess {
+    @Prop({ required: true })
+    shortcode: String;
+
+    @Prop()
+    caption: String;
+
+    @Prop()
+    time_start: String;
+
+    @Prop()
+    time_stop: String;
+
+    @Prop({ required: true })
+    user_id: String;
+
+    @Prop({ required: true })
+    task_id: String;
+}
+
+export const TaskSuccessSchema = SchemaFactory.createForClass(TaskSuccess);
